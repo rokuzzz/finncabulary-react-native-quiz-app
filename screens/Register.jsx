61482f389e5a46d1a7635032ef8b67ff13387e5c
Roman Kuzero <rombeldan@gmail.com>
2023-08-28 16:16:47 +0300

    feat: Create Register Screen
    - Create Register screen using reusable components from Login screen
    - Add BackArrowButton, CustomInput, and CustomButton components
    - Add state for username, email, and password
    - Add basic onPress function for register button

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,13 +1,93 @@
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Ionicons, } from '@expo/vector-icons';
 
-import React from 'react'
+import CustomInput from '../components/authentication/CustomInput';
+import CustomButton from '../components/authentication/CustomButton';
+import BackArrowButton from '../components/common/BackArrowButton';
 
-const Register = () => {
+const Register = ({navigation}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onRegisterPressed = () => {
+    console.warn('Register')
+  }
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <BackArrowButton navigation={navigation} />
 
-export default Register
+        <Text style={styles.title}>Register</Text>
+        <Text style={styles.subtitle}>
+          {'Enter your details\nto create a new account'}
+        </Text>
+
+        <CustomInput 
+          placeholder='Username' 
+          value={username} 
+          setValue={setUsername} 
+        />
+        <CustomInput 
+          placeholder='Email' 
+          value={username} 
+          setValue={setUsername} 
+        />
+        <CustomInput 
+          placeholder='Password' 
+          value={password} 
+          setValue={setPassword} 
+          secureTextEntry
+        />
+        <CustomInput 
+          placeholder='Confirm Password' 
+          value={password} 
+          setValue={setPassword} 
+          secureTextEntry
+        />
+
+        <View style={{marginTop: 40, width: '100%'}}>
+          <CustomButton text='Register' onPress={onRegisterPressed} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    paddingHorizontal: 15,
+    marginBottom: 10,
+
+    fontSize: 40,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+
+    color: '#000080',
+    textShadowColor: 'rgba(0, 0, 0, 0.2)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+  },
+  subtitle: {
+    paddingHorizontal: 15,
+    marginBottom: 40,
+    
+    fontSize: 18,
+    textAlign: 'center',
+
+    color: '#000080',
+  },
+});
+
+export default Register;
